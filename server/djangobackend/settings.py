47c964aca1a3b1a826464c539cddee19cb781586
Muaz Abdeen <muaz.abdeen@cs.huji.ac.il>
2023-07-06 21:26:19 +0300

    complete lab1: about & contact

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -123,5 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(STATIC_ROOT, 'djangoapp')
+]
 MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
 MEDIA_URL = '/media/'
