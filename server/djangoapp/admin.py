e691be32e7849d9144765947e5408ba20df193e3
Muaz Abdeen <muaz.abdeen@cs.huji.ac.il>
2023-07-09 20:18:50 +0300

    Build CarModel and CarMake Django Models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,29 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
+class CarModelInline(admin.StackedInline):
+  model = CarModel
+  extra = 3
 
-# CarModelInline class
+class CarModelAdmin(admin.ModelAdmin):
+  list_display = ('dealer_id', 'type', 'get_maker','name', 'year')
+  list_filter = ['dealer_id','type', 'year']
+  search_fields = ['dealer_id', 'type', 'get_maker','name', 'year']
+  
+  def get_maker(self, obj):
+    return obj.car_make.name
+  get_maker.admin_order_field = 'car_make'
+  get_maker.short_description = 'Car Maker'
+  
 
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+  inlines =  [CarModelInline]
+  list_display = ('name','description')
+  list_filter = ['name']
+  search_fields = ['name']
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,24 +1,40 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.utils.timezone import now
 
 
 # Create your models here.
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30)
+    description = models.CharField(max_length=1000)
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return f'Name: {self.name}\n Description: {self.description}'
 
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'sedan'
+    SUV = 'SUV'
+    PICKUP = 'pickup'
+    VAN = 'van'
+    WAGON = 'wagon'
+    CAR_TYPES = [
+		(SEDAN, 'Sedan'),
+		(SUV, 'SUV'),
+		(PICKUP, 'Pickup'),
+		(VAN, 'Van'),
+		(WAGON, 'Wagon')
+	]
+    
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=30)
+    dealer_id = models.IntegerField(null=False,
+                                    validators=[MaxValueValidator(20), MinValueValidator(1)])
+    type = models.CharField(max_length=8, choices=CAR_TYPES, default=SEDAN)
+    year = models.DateField(null=False, default=now)
+    
+    def __str__(self):
+        return f'{self.car_make.name} {self.name} ({self.type}), {self.year}.\n' + \
+               f'Dealer_ID = {self.dealer_id}.'
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
